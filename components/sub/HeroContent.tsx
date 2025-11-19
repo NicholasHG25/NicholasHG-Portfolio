df96785c56a4bb2e3cc03ab4cd2987b916594963
NicholasHG25 <nicholashg0248@gmail.com>
2025-11-19 13:52:51 +0700

    fixed cv

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -59,7 +59,6 @@ export const HeroContent = () => {
         <motion.a
           variants={slideInFromLeft(1)}
           href="/NicholasHG_CV.pdf"
-          download
           className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px] mx-auto lg:mx-0"
         >
           Download CV
